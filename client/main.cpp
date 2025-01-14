31188b3ee0fdb27f0611c9da12907fb6efbf2bad
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-01-14 16:07:39 +0200

    Added connection loop (send/recv). Added handling for server unexpected socket termination. Added a way to get only the msg part from recv buffer instead of passing the whole buffer as the msg (assuming the buffer did not capture the msg partially). Moved CloseSocket() and CloseWinsockDll() to the try block. Created an input handling thread for non-blocking I/O. Added mutex thread locking when outputing to the console to prevent interference with the input buffer. Added a check after the connection gets closed to wait the input thread before closing shared resources

--- client/main.cpp
+++ client/main.cpp
@@ -1,22 +1,131 @@
-#include "client.h"
+#include <WinSock2.h>
 #include <iostream>
+#include "client.h"
+#include "../helper.h"
+#include <string>
 #include <stdexcept>
+// threading
+#include <thread>
+#include <mutex>
+
+SOCKET conn;
+bool isSocketOpen;
+std::mutex ioMutex;
+
+void HandleUserInputLoop()
+{
+	std::string input;
+	// user input loop
+	while (isSocketOpen)
+	{
+		{
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "Input: ";
+		}
+		// get input
+		std::getline(std::cin, input);
+		// close socket
+		if (input == helper::CLOSE_SOCKET_KEY)
+		{
+			bool ok = SendMsg(conn, helper::CLOSE_SOCKET_MSG);
+			if (!ok)
+			{
+				// lock writing output to console
+				std::lock_guard<std::mutex> lock(ioMutex);
+				std::cout << "Failed to send close socket msg" << std::endl;
+				continue;
+			}
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "Sent close socket msg" << std::endl;
+			break;
+		}
+	}
+	// lock writing output to console
+	std::lock_guard<std::mutex> lock(ioMutex);
+	std::cout << "User input loop closed" << std::endl;
+}
 
 int main()
 {
-	ClientSocket conn = ClientSocket();
+	ClientSocket clientSocket = ClientSocket("192.168.2.105", 1234);
 	try 
 	{
-		conn.InitWinsockDll();
-		conn.Create();
-		conn.Connect("192.168.2.105", 1234);
+		InitWinsockDll();
+		conn = clientSocket.CreateAndConnect();
+		isSocketOpen = true;
+		{
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "Connection established!" << std::endl;
+		}
+		// create 2nd thread to handle recv loop
+		std::thread userInputLoopThread(HandleUserInputLoop);
+		// recv buffer
+		char recvBuffer[helper::RECV_BUFFER_LEN];
+		while (isSocketOpen)
+		{
+			int recvByteCount = recv(conn, recvBuffer, helper::RECV_BUFFER_LEN, 0);
+			if (recvByteCount < 0)
+			{
+				// lock writing output to console
+				std::lock_guard<std::mutex> lock(ioMutex);
+				std::cout << "Failed to receive msg. Error code: " << WSAGetLastError() << std::endl;
+				isSocketOpen = false;
+				continue;
+			}
+			{
+				// lock writing output to console
+				std::lock_guard<std::mutex> lock(ioMutex);
+				std::cout << "Received " << recvByteCount << " bytes from a " << helper::RECV_BUFFER_LEN << " bytes buffer" << std::endl;
+			}
+			// get msg from recv buffer
+			int bufferCharCount = sizeof(recvBuffer) / sizeof(recvBuffer[0]);
+			std::string recvMsg = GetRecvBufferMsg(recvBuffer, bufferCharCount, recvByteCount);
+			{
+				// lock writing output to console
+				std::lock_guard<std::mutex> lock(ioMutex);
+				std::cout << "Received msg: " << recvMsg << std::endl;
+			}
+			// close socket msg
+			if (recvMsg == helper::CLOSE_SOCKET_MSG)
+			{
+				isSocketOpen = false;
+				continue;
+			}
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "SERVER: " << recvMsg << std::endl;
+		}
+		{
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "Recv loop closed" << std::endl;
+		}
+		// Check thread state before joining
+        if (userInputLoopThread.joinable()) 
+		{
+			// Wait for the input thread to finish
+            userInputLoopThread.join();
+		}
+		clientSocket.Close();
+		{
+			// lock writing output to console
+			std::lock_guard<std::mutex> lock(ioMutex);
+			std::cout << "Client socket closed" << std::endl;
+		}
+		CloseWinsockDll();
+		// lock writing output to console
+		std::lock_guard<std::mutex> lock(ioMutex);
+		std::cout << "Winsock dll closed" << std::endl;
 	} 
 	catch (std::runtime_error e) 
 	{
+		isSocketOpen = false;
+		// lock writing output to console
+        std::lock_guard<std::mutex> lock(ioMutex);
 		std::cout << "An exception occured: " << e.what() << std::endl;
-		return -1;
 	}
-	conn.Close();
-	conn.CloseWinsockDll();
 	return 0;
 }
