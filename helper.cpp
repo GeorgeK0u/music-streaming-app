3eb8b933dbae3e0adbf48eb738c0178d4291123d
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-03-10 22:26:45 +0200

    Replaced strings with const char*. Added a SendChunkMsg function to differentiate between SendMsg which the msg size can be found with strlen and SendChunkMsg that I have to provide the chunk size. Removed GetRecvBufferMsg

--- helper.cpp
+++ helper.cpp
@@ -1,9 +1,13 @@
 #include <WinSock2.h>
-#include <iostream>
 #include "helper.h"
 #include <stdexcept>
 
-void InitWinsockDll()
+const char* helper::CLOSE_SOCKET_MSG = "CloseSocket";
+const char* helper::START_STREAMING_MSG = "StartStreaming";
+const char* helper::STREAMING_FINISHED_MSG = "StreamingFinished";
+const char* helper::SEND_CHUNK_MSG = "SendChunk";
+
+void helper::InitWinsockDll()
 {
 	// winsock version to use
 	WORD wVersion = MAKEWORD(2,2);
@@ -16,54 +20,23 @@ void InitWinsockDll()
 		throw std::runtime_error("The winsock dll not found");
 	}
 }
-void CloseWinsockDll()
-{
-	// close winsock dll
-	int res = WSACleanup();
-	if (res == SOCKET_ERROR)
-	{
-		throw std::runtime_error("The winsock dll not found");
-	}
-}
 
-bool SendMsg(SOCKET conn, std::string msg, bool showOutput)
+void helper::SendMsg(SOCKET conn, const char* msg)
 {
-	int msgByteCount = msg.length();
-	int byteSentCount = send(conn, msg.c_str(), msgByteCount, 0);
+	int msgByteCount = strlen(msg);
+	SendChunkMsg(conn, msg, msgByteCount);
+}
+void helper::SendChunkMsg(SOCKET conn, const char* msg, int msgByteCount)
+{
+	int byteSentCount = send(conn, msg, msgByteCount, 0);
 	// failed to send msg
 	if (byteSentCount == SOCKET_ERROR)
 	{
-		if (showOutput)
-		{
-			std::cout << "Failed to send msg: " << msg << std::endl;
-		}
-		return false;
+		throw std::runtime_error("Failed to send msg");
 	}
-	if (showOutput)
-	{
-		std::cout << "Sent msg: " << msg << std::endl;
-	}
-	return true;
 }
 
-std::string GetRecvBufferMsg(char recvBuffer[], int bufferCharCount, int recvByteCount)
-{
-	std::string recvMsg = "";
-	int chCount = 0;
-	for (int i = 0; i < bufferCharCount; i++)
-	{
-		char ch = recvBuffer[i];
-		recvMsg += ch;
-		chCount++;
-		if (chCount == recvByteCount)
-		{
-			break;
-		}
-	}
-	return recvMsg;
-}
-
-void CloseSocket(SOCKET conn)
+void helper::CloseSocket(SOCKET conn)
 {
 	if (conn == INVALID_SOCKET)
 	{
@@ -74,5 +47,15 @@ void CloseSocket(SOCKET conn)
 	if (res == SOCKET_ERROR)
 	{
 		throw std::runtime_error("Failed to close socket");
+	}
+}
+
+void helper::CloseWinsockDll()
+{
+	// close winsock dll
+	int res = WSACleanup();
+	if (res == SOCKET_ERROR)
+	{
+		throw std::runtime_error("The winsock dll not found");
 	}
 }
