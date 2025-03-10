f2c0197389ad3f1e718ada4a47a6548d0f4291c3
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-03-10 21:20:57 +0200

    Added chunk sending function. On recv loop added handling if client socket closes, StartStreaming and SendChunk msg and replaced strings with const char*

--- server/main.cpp
+++ server/main.cpp
@@ -1,66 +1,103 @@
-#include <WinSock2.h>
-#include <iostream>
 #include "server.h"
-#include "../helper.h"
+#include "fileHandler.h"
+#include "..\helper.h"
+#include <iostream>
 #include <stdexcept>
 
-SOCKET conn;
-bool isSocketOpen;
+SOCKET conn = INVALID_SOCKET;
+FileHandler fileHandler(helper::RECV_BUFFER_LEN);
+
+void SendChunk()
+{
+	char* chunkDataArrPtr = fileHandler.ReadChunk();
+	// streaming finished
+	if (chunkDataArrPtr == nullptr)
+	{
+		return;
+	}
+	// send chunk
+	helper::SendChunkMsg(conn, chunkDataArrPtr, fileHandler.GetChunkSize());
+	std::cout << "Sent chunk" << std::endl;
+	delete[] chunkDataArrPtr;
+}
 
 int main()
 {
 	int maxConnNum = 1;
-	ServerSocket listenSocket = ServerSocket("192.168.2.105", 1234, maxConnNum);
+	ServerSocket serverSocket = ServerSocket("192.168.2.105", 1234, maxConnNum);
 	try 
 	{
-		InitWinsockDll();
-		conn = listenSocket.InitAndAccept();
-		isSocketOpen = true;
+		conn = serverSocket.CreateAndAccept();
+		std::cout << "Client accepted" << std::endl;
+		bool isSocketOpen = true;
 		// conn loop
 		char recvBuffer[helper::RECV_BUFFER_LEN];
 		while (isSocketOpen)
 		{
 			int recvByteCount = recv(conn, recvBuffer, helper::RECV_BUFFER_LEN, 0);
-			if (recvByteCount < 0)
+			if (recvByteCount <= 0)
 			{
-				std::cout << "Failed to receive msg. Error code: " << WSAGetLastError() << std::endl;
+				if (recvByteCount < 0)
+				{
+					std::cout << "Failed to receive msg. Error code: " << WSAGetLastError() << std::endl;
+				}
 				isSocketOpen = false;
 				continue;
 			}
-			std::cout << "Received: " << recvByteCount << " bytes from a " << helper::RECV_BUFFER_LEN << " bytes buffer" << std::endl;
 			// get msg from recv buffer
-			int bufferCharCount = sizeof(recvBuffer) / sizeof(recvBuffer[0]);
-			std::string recvMsg = GetRecvBufferMsg(recvBuffer, bufferCharCount, recvByteCount);
-			std::cout << "Received msg: " << recvMsg << std::endl;
-			// close socket msg
-			if (recvMsg == helper::CLOSE_SOCKET_MSG)
+			std::string recvMsgStr = "";
+			for (int i = 0; i < recvByteCount; i++)
 			{
-				bool ok = SendMsg(conn, helper::CLOSE_SOCKET_MSG, true);
-				if (ok)
-				{
-					isSocketOpen = false;
-				}
-				continue;
+				char ch = recvBuffer[i];
+				recvMsgStr += ch;
 			}
-			std::cout << "CLIENT: " << recvMsg << std::endl;
+			const char* recvMsg = recvMsgStr.c_str();
+			// close socket msg
+			if (strcmp(recvMsg, helper::CLOSE_SOCKET_MSG) == 0)
+			{
+				helper::SendMsg(conn, helper::CLOSE_SOCKET_MSG);
+				isSocketOpen = false;
+			}
+			else if (strcmp(recvMsg, helper::START_STREAMING_MSG) == 0)
+			{
+				std::cout << "Start streaming msg received from client" << std::endl;
+				// path is relative to .output dir
+				/* const char* fileName = "songs/nft-song.wav"; */
+				const char* fileName = "songs/track1.wav";
+				fileHandler.CheckReadFile(fileName);
+				// send chunk
+				SendChunk();
+			}
+			else if (strcmp(recvMsg, helper::SEND_CHUNK_MSG) == 0)
+			{
+				std::cout << "Client requested next chunk" << std::endl;
+				// send chunk
+				SendChunk();
+			}
+			else
+			{
+				std::cout << "CLIENT: " << recvMsg << std::endl;
+			}
 		}
-		std::cout << "Recv loop closed" << std::endl;
 	} 
 	catch (std::runtime_error e) 
 	{
 		std::cout << "An exception occured: " << e.what() << std::endl;
 	}
-	// clean up 
+	// cleanup
 	try
 	{
-		CloseSocket(conn);
-		std::cout << "Server socket closed" << std::endl;
-		CloseWinsockDll();
-		std::cout << "Winsock dll closed" << std::endl;
+		// release file memory
+		fileHandler.Release();
+		std::cout << "File memory released" << std::endl; 
+		// release socket memory
+		serverSocket.Close();
+		conn = INVALID_SOCKET;
+		std::cout << "Server socket memory released" << std::endl; 
 	}
 	catch (std::runtime_error e)
 	{
-		std::cout << "An exception occured on cleanup: " << e.what() << std::endl;
+		std::cout << "A cleanup exception occured: " << e.what() << std::endl;
 	}
 	return 0;
 }
