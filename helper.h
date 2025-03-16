41e890d07c1399311ca6e0750ac59a2c9797154c
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-03-16 11:50:18 +0200

    Replaced socket variable argument in CloseSocket to copy by ref. Added reading connection values from txt file and get recv msg from recv buffer

--- helper.h
+++ helper.h
@@ -9,9 +9,16 @@ class helper
 		static const char* START_STREAMING_MSG;
 		static const char* STREAMING_FINISHED_MSG;
 		static const char* SEND_CHUNK_MSG;
+		static void GetSocketConfig(const char*& ipv4Addr, int& port);
 		static void InitWinsockDll();
 		static void SendMsg(SOCKET conn, const char* msg);
 		static void SendChunkMsg(SOCKET conn, const char* msg, int msgByteCount);
-		static void CloseSocket(SOCKET conn);
+		static void GetRecvMsg(char recvBuffer[], int recvByteCount, char recvMsg[]);
+		static void CloseSocket(SOCKET& conn);
 		static void CloseWinsockDll();
+	private:
+		static const char* ipv4Addr;
+		static int ipv4AddrLength;
+		static int port;
+		static void ReadSocketConfig();
 };
