ace533580699bc45abeb5b258975c958e6563869
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2025-03-16 11:52:40 +0200

    Replaced socket variable argument in CloseSocket to copy by ref. Added reading connection values from txt file and get recv msg from recv buffer

--- helper.cpp
+++ helper.cpp
@@ -1,11 +1,47 @@
 #include <WinSock2.h>
 #include "helper.h"
 #include <stdexcept>
+#include <fstream>
+#include <string>
 
 const char* helper::CLOSE_SOCKET_MSG = "CloseSocket";
 const char* helper::START_STREAMING_MSG = "StartStreaming";
 const char* helper::STREAMING_FINISHED_MSG = "StreamingFinished";
 const char* helper::SEND_CHUNK_MSG = "SendChunk";
+const char* helper::ipv4Addr = nullptr;
+int helper::ipv4AddrLength = 0;
+int helper::port = 0;
+
+void helper::GetSocketConfig(const char*& ipv4Addr, int& port)
+{
+	if (helper::ipv4Addr == nullptr)
+	{
+		ReadSocketConfig();
+	}
+	ipv4Addr = new char[helper::ipv4AddrLength];
+	memcpy((void*)ipv4Addr, helper::ipv4Addr, helper::ipv4AddrLength);
+	port = helper::port;
+}
+
+void helper::ReadSocketConfig()
+{
+	std::ifstream file;
+	file.open("../socket-config.txt");
+	if (!file.is_open())
+	{
+		throw std::runtime_error("Failed to open socket-config txt file");
+	}
+	std::string ipv4Addr, portStr;
+	getline(file, ipv4Addr);
+	// +1 for the null byte
+	helper::ipv4AddrLength = ipv4Addr.length()+1;
+	helper::ipv4Addr = new char[helper::ipv4AddrLength];
+	memcpy((void*)helper::ipv4Addr, ipv4Addr.c_str(), helper::ipv4AddrLength);
+	getline(file, portStr);
+	file.close();
+	int port = std::stoi(portStr);
+	helper::port = port;
+}
 
 void helper::InitWinsockDll()
 {
@@ -36,7 +72,16 @@ void helper::SendChunkMsg(SOCKET conn, const char* msg, int msgByteCount)
 	}
 }
 
-void helper::CloseSocket(SOCKET conn)
+void helper::GetRecvMsg(char recvBuffer[], int recvByteCount, char recvMsg[])
+{
+	for (int i = 0; i < recvByteCount; i++)
+	{
+		char ch = recvBuffer[i];
+		recvMsg[i] = ch;
+	}
+}
+
+void helper::CloseSocket(SOCKET& conn)
 {
 	if (conn == INVALID_SOCKET)
 	{
@@ -44,10 +89,12 @@ void helper::CloseSocket(SOCKET conn)
 	}
 	// close socket
 	int res = closesocket(conn);
+	// failed to close socket
 	if (res == SOCKET_ERROR)
 	{
 		throw std::runtime_error("Failed to close socket");
 	}
+	conn = INVALID_SOCKET;
 }
 
 void helper::CloseWinsockDll()
@@ -58,4 +105,6 @@ void helper::CloseWinsockDll()
 	{
 		throw std::runtime_error("The winsock dll not found");
 	}
+	// delete ip dynamic memory
+	delete[] helper::ipv4Addr;
 }
